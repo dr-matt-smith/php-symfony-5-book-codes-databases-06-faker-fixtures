0858c2e5a4e29e418ec70bcda8256664eb294b35
dr-matt-smith <matt.smith@itb.ie>
2020-02-11 09:47:55 +0000

    added files to project

---
+++ src/DataFixtures/StudentFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use App\Entity\Student;
+use Faker\Factory;
+
+class StudentFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        $numStudents = 10;
+        for ($i=0; $i < $numStudents; $i++) {
+            $firstName = $faker->firstNameMale;
+            $surname = $faker->lastName;
+
+            $student = new Student();
+            $student->setFirstName($firstName);
+            $student->setSurname($surname);
+
+            $manager->persist($student);
+        }
+
+
+        $manager->flush(); }
+
+
+}
